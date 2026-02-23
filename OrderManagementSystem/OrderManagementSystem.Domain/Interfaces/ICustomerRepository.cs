f2393040ecaae7168bf1f42ec57bc5d099071585
mubu002 <mustafa_buga1@hotmail.com>
2026-02-23 14:45:28 +0100

    Added repository interfaces to domain layer

---
+++ OrderManagementSystem/OrderManagementSystem.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderManagementSystem.Domain.Interfaces;
+
+using OrderManagementSystem.Domain.Entities;
+
+public interface ICustomerRepository
+{
+    Customer? GetById(int id);
+    IEnumerable<Customer> GetAll();
+    void Add(Customer customer);
+    void Update(Customer customer);
+    void Delete(int id);
+}
---
+++ OrderManagementSystem/OrderManagementSystem.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderManagementSystem.Domain.Interfaces;
+
+using OrderManagementSystem.Domain.Entities;
+
+public interface IProductRepository
+{
+    Product? GetById(int id);
+    IEnumerable<Product> GetAll();
+    void Add(Product product);
+    void Update(Product product);
+    void Delete(int id);
+}
