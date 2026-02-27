c90cb22f8ce7650dc762bc4b9c277560199ac7a8
mubu002 <mustafa_buga1@hotmail.com>
2026-02-27 08:41:53 +0100

    Added OrderService unit test using Moq to verify dependency isolation

---
+++ OrderManagementSystem/OrderManagementSystem.Tests/OrderServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Moq;
+using OrderManagementSystem.Application.Services;
+using OrderManagementSystem.Domain.Interfaces;
+
+namespace OrderManagementSystem.Tests;
+
+public class OrderServiceTests
+{
+    [Test]
+    public void CreateOrder_ShouldThrow_WhenCustomerDoesNotExist()
+    {
+        // Arrange
+        var mockOrderRepo = new Mock<IOrderRepository>();
+        var mockCustomerRepo = new Mock<ICustomerRepository>();
+        var mockProductRepo = new Mock<IProductRepository>();
+
+        // Simulate that customer does not exist
+        mockCustomerRepo
+            .Setup(repo => repo.GetById(It.IsAny<int>()))
+            .Returns((Domain.Entities.Customer?)null);
+
+        var service = new OrderService(
+            mockOrderRepo.Object,
+            mockCustomerRepo.Object,
+            mockProductRepo.Object);
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            service.CreateOrder(1, 999);
+        });
+    }
+}
