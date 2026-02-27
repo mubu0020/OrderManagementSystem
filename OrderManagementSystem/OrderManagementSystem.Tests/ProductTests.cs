38da74c04d1992f2b84c168e77240075dbafe673
mubu002 <mustafa_buga1@hotmail.com>
2026-02-27 08:34:53 +0100

    Added Product domain unit tests

---
+++ OrderManagementSystem/OrderManagementSystem.Tests/ProductTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OrderManagementSystem.Domain.Entities;
+
+namespace OrderManagementSystem.Tests;
+
+public class ProductTests
+{
+    [Test]
+    public void Constructor_ShouldThrow_WhenPriceIsNegative()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var product = new Product(1, "Laptop", -100, 10);
+        });
+    }
+
+    [Test]
+    public void Constructor_ShouldThrow_WhenStockIsNegative()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var product = new Product(1, "Laptop", 100, -5);
+        });
+    }
+
+    [Test]
+    public void DecreaseStock_ShouldThrow_WhenStockBecomesNegative()
+    {
+        var product = new Product(1, "Laptop", 1000, 5);
+
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            product.DecreaseStock(10);
+        });
+    }
+
+    [Test]
+    public void IncreaseStock_ShouldIncreaseStockQuantity()
+    {
+        var product = new Product(1, "Laptop", 1000, 5);
+
+        product.IncreaseStock(5);
+
+        Assert.That(product.StockQuantity, Is.EqualTo(10));
+    }
+}
